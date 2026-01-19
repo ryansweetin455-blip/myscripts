de6ca9a21fb972d51ff5c41dde00b251971b0132
ryansweetin455-blip <ryansweetin455@gmail.com>
2026-01-18 21:57:59 -0300

    Update open.lua

--- open.lua
+++ open.lua
@@ -1,4 +1,3 @@
-
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
@@ -261,3 +260,4 @@ MovementTab:CreateButton({
 		end
 	end
 })
+
